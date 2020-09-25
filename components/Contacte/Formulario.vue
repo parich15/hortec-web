bfb2b99eea708bc5d5bafebcaf6faace399452b4
Parich15 <oscarparicio8@gmail.com>
2020-09-25 10:44:25 +0200

    cambio en data-netlify

--- components/Contacte/Formulario.vue
+++ components/Contacte/Formulario.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ValidationObserver v-slot="{ pristine, invalid, passed }">
-      <b-form v-if="show" method="post" netlify @submit.prevent="onSubmit" @reset="onReset">
+      <b-form v-if="show" method="post" data-netlify="true" @submit.prevent="onSubmit" @reset="onReset">
         <input type="hidden" name="form-name" value="contact">
         <ValidationProvider
           v-slot="{ errors }"
