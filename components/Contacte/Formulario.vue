45572f64766164ee1fc627511046531dc637d180
Parich15 <oscarparicio8@gmail.com>
2020-09-25 12:30:56 +0200

    Pruebas

--- components/Contacte/Formulario.vue
+++ components/Contacte/Formulario.vue
@@ -1,141 +1,136 @@
 <template>
-  <div>
-    <b-form
-      class="formulario"
-      name="contacto"
-      method="post"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-      @submit.prevent="onSubmit"
-      @reset="onReset"
-    >
-      <ValidationObserver v-slot="{ pristine, invalid, passed }">
-        <input type="hidden" name="form-name" value="contacto">
-        <ValidationProvider
-          v-slot="{ errors }"
-          name="Nombre"
-          rules="required|txtmin:2|txtmax:20"
+  <b-form
+    class="formulario"
+    name="contacto"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+    @submit.prevent="handleSubmit()"
+    @reset="onReset"
+  >
+    <ValidationObserver v-slot="{ pristine, invalid, passed }">
+      <input type="hidden" name="form-name" value="contacto">
+      <ValidationProvider
+        v-slot="{ errors }"
+        name="Nombre"
+        rules="required|txtmin:2|txtmax:20"
+      >
+        <b-form-group
+          id="nom-contacto"
+          label="Com et dius?"
+          label-for="nombre"
         >
-          <b-form-group
-            id="nom-contacto"
-            label="Com et dius?"
-            label-for="nombre"
-          >
-            <b-form-input
-              id="nombre"
-              v-model="formulario.nom"
-              type="text"
-              name="Nombre"
-              required
-              placeholder="Digan's el teu nom"
-            />
-            <span class="input-invalid-message">
-              {{ errors[0] }}
-            </span>
-          </b-form-group>
-        </ValidationProvider>
+          <b-form-input
+            id="nombre"
+            v-model="formulario.nom"
+            type="text"
+            name="Nombre"
+            required
+            placeholder="Digan's el teu nom"
+          />
+          <span class="input-invalid-message">
+            {{ errors[0] }}
+          </span>
+        </b-form-group>
+      </ValidationProvider>
 
-        <ValidationProvider v-slot="{errors}" rules="required|txtmax:30" name="email">
-          <b-form-group
-            id="email-contacto"
-            label="Quin es el teu correu?:"
-            label-for="email"
-          >
-            <b-form-input
-              id="email"
-              v-model="formulario.email"
-              type="email"
-              name="email"
-              required
-              placeholder="Digan's el teu email"
-            />
-            <span class="input-invalid-message">
-              {{ errors[0] }}
-            </span>
-          </b-form-group>
-        </ValidationProvider>
+      <ValidationProvider v-slot="{errors}" rules="required|txtmax:30" name="email">
+        <b-form-group
+          id="email-contacto"
+          label="Quin es el teu correu?:"
+          label-for="email"
+        >
+          <b-form-input
+            id="email"
+            v-model="formulario.email"
+            type="email"
+            name="email"
+            required
+            placeholder="Digan's el teu email"
+          />
+          <span class="input-invalid-message">
+            {{ errors[0] }}
+          </span>
+        </b-form-group>
+      </ValidationProvider>
 
-        <ValidationProvider v-slot="{ errors }" rules="minmax:9,9|required" name="telefono">
-          <b-form-group
-            id="telefon-contacte"
-            label="El teu numero de telèfon:"
-            label-for="telf"
-            description="Mai compartirem les teves dades amb ningú"
-          >
-            <b-form-input
-              id="telf"
-              v-model="formulario.tlf"
-              name="telefono"
-              required
-              placeholder="Per últim, el teu telèfon"
-              type="number"
-            />
-            <span class="input-invalid-message">
-              {{ errors[0] }}
-            </span>
-          </b-form-group>
-        </ValidationProvider>
+      <ValidationProvider v-slot="{ errors }" rules="minmax:9,9|required" name="telefono">
+        <b-form-group
+          id="telefon-contacte"
+          label="El teu numero de telèfon:"
+          label-for="telf"
+          description="Mai compartirem les teves dades amb ningú"
+        >
+          <b-form-input
+            id="telf"
+            v-model="formulario.tlf"
+            name="telefono"
+            required
+            placeholder="Per últim, el teu telèfon"
+            type="number"
+          />
+          <span class="input-invalid-message">
+            {{ errors[0] }}
+          </span>
+        </b-form-group>
+      </ValidationProvider>
 
-        <ValidationProvider v-slot="{ errors }" rules="cp:5,5|required" name="CP">
-          <b-form-group
-            id="codi-postal"
-            label="Codi Postal"
-            label-for="CP"
-          >
-            <b-form-input
-              id="CP"
-              v-model="formulario.cp"
-              required
-              placeholder="D'on ets?"
-              type="number"
-              name="CP"
-            />
-            <span class="input-invalid-message">
-              {{ errors[0] }}
-            </span>
-          </b-form-group>
-        </ValidationProvider>
+      <ValidationProvider v-slot="{ errors }" rules="cp:5,5|required" name="CP">
+        <b-form-group
+          id="codi-postal"
+          label="Codi Postal"
+          label-for="CP"
+        >
+          <b-form-input
+            id="CP"
+            v-model="formulario.cp"
+            required
+            placeholder="D'on ets?"
+            type="number"
+            name="CP"
+          />
+          <span class="input-invalid-message">
+            {{ errors[0] }}
+          </span>
+        </b-form-group>
+      </ValidationProvider>
 
-        <ValidationProvider v-slot="{errors}" rules="required">
-          <b-form-group id="preguntes">
-            <b-form-radio-group id="radiobuttons" v-model="formulario.seleccion">
-              <b-form-radio value="Informació" name="info">
-                Vull més Informació
-              </b-form-radio>
-              <b-form-radio value="Sol·liciutd d'alta" name="client">
-                Vull fer me client
-              </b-form-radio>
-            </b-form-radio-group>
-            <span class="input-invalid-message">
-              {{ errors[0] }}
-            </span>
-          </b-form-group>
-        </ValidationProvider>
-        <!-- <div>
+      <ValidationProvider v-slot="{errors}" rules="required">
+        <b-form-group id="preguntes">
+          <b-form-radio-group id="radiobuttons" v-model="formulario.seleccion">
+            <b-form-radio value="Informació" name="info">
+              Vull més Informació
+            </b-form-radio>
+            <b-form-radio value="Sol·liciutd d'alta" name="client">
+              Vull fer me client
+            </b-form-radio>
+          </b-form-radio-group>
+          <span class="input-invalid-message">
+            {{ errors[0] }}
+          </span>
+        </b-form-group>
+      </ValidationProvider>
+      <!-- <div>
           <select id="ciudades" v-model="regionSeleccionada" name="Ciudad">
             <option v-for="region in regiones[0]" :key="region.isoCode" :value="region.name">
               {{ region.name }}
             </option>
           </select>
         </div> -->
-        <b-button type="submit" variant="primary" :class="{'disabled':pristine || invalid || !passed }" :disabled="pristine|| invalid || !passed">
-          Enviar
-        </b-button>
-        <b-button type="reset" variant="outline-danger">
-          Esborra
-        </b-button>
-      </ValidationObserver>
-    </b-form>
-
-    <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ formulario }}</pre>
-    </b-card>
-  </div>
+      <b-button type="submit" variant="primary" :class="{'disabled':pristine || invalid || !passed }" :disabled="pristine|| invalid || !passed">
+        Enviar
+      </b-button>
+      <b-button type="reset" variant="outline-danger">
+        Esborra
+      </b-button>
+    </ValidationObserver>
+  </b-form>
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
+
   name: 'Formulario',
   data () {
     return {
@@ -172,6 +167,26 @@ export default {
       this.$nextTick(() => {
         this.show = true
       })
+    },
+    encode (data) {
+      return Object.keys(data)
+        .map(
+          key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join('&')
+    },
+    handleSubmit () {
+      const axiosConfig = {
+        header: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      }
+      axios.post(
+        '/Contacte',
+        this.encode({
+          'form-name': 'contacto',
+          ...this.formulario
+        }),
+        axiosConfig
+      )
     }
   }
 
