5a2cf714b75c93af07b102759a0edee0db270583
parich15 <oscarparicio8@gmail.com>
2020-09-30 13:52:14 +0200

    SCSS Blog

---
+++ components/Blog/BotonBack.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="cajaBtn">
+    <b-button pill class="botonBlog fade-in" to="/Blog">
+      <b-icon icon="arrow-counterclockwise" aria-hidden="true" />
+      Torna Enrere
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BotonBack'
+}
+</script>
+
+<style lang="scss">
+
+</style>
